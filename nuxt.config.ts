ca618e3a1df306563fb66a4506ceb1f7ec711725
Michał Łukasik <michal.lukasik@cloudflight.io>
2026-02-12 12:46:31 +0100

    deploys to Cloudflare wrangler

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare'
+  },
   css: ['./app/assets/css/main.css'],
   vite: {
     plugins: [
